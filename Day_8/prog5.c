f6a7d51ef2d102e30d69f3e054494150caba4502
abhishek-kumbhar <57053104+abhishek-kumbhar@users.noreply.github.com>
2020-03-26 09:24:25 +0530

    Create prog5.c

---
+++ Day_8/prog5.c
@@ -0,0 +1,28 @@
+/*
+
+Program 5: Write a Program to print following Pattern.
+            *
+            *  *
+            *  *  *
+            *  *  *  *
+
+*/
+
+extern int printf (const char *, ...);
+extern int scanf (const char *, ...);
+
+void main (void) {
+
+    int x;
+
+    printf ("\nEnter no of Rows : ");
+    scanf ("%d", &x);
+
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < i + 1; j++) {
+            printf("*  ");
+        }
+        printf("\n");
+    }
+
+}
