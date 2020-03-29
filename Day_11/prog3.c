0de56d3f96dde3661a03aac2c23bd5528c58a53e
abhishek-kumbhar <57053104+abhishek-kumbhar@users.noreply.github.com>
2020-03-29 08:26:23 +0530

    Create prog3.c

---
+++ Day_11/prog3.c
@@ -0,0 +1,23 @@
+/*
+
+Program 3: Write a Program to print Cubes and Squares of all Even numbers
+           ranges between given input number.
+
+
+*/
+
+extern int printf (const char *, ...);
+extern int scanf (const char *, ...);
+
+void main (void) {
+    
+    int x;
+    
+    printf("\nEnter a number : ");
+    scanf("%d", &x);
+
+    for (int i = 1; i < x + 1; i++) {
+        (i % 2 == 0) ? printf("\nCube of %2d : %4d and Square of %2d : %3d", i, i * i * i, i, i * i) : 1;
+    }
+
+}
