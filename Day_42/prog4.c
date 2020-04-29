91223bd3df98c87714fed90d0955e9fe1cce58af
Abhishek Kumbhar <57053104+abhishek-kumbhar@users.noreply.github.com>
2020-04-29 23:26:51 +0530

    Create prog4.c

---
+++ Day_42/prog4.c
@@ -0,0 +1,40 @@
+/*
+
+Program 4: Write a Program to Print following Pattern.
+
+                        *         
+                      * * *       
+                    * * * * *     
+                  * * * * * * *   
+                * * * * * * * * * 
+
+
+*/
+
+# include <stdio.h>
+
+void printPattern (int rows) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < (2 * rows - 1); j++) {
+            if (i + j > rows - 2 && i + j < rows + 2 * i){
+                printf ("* ");
+            } else {
+                printf ("  ");
+            }
+        }
+        printf ("\n");
+    }
+
+}
+
+
+void main (void) {
+
+    int rows;
+    printf ("\nEnter number of Rows : ");
+    scanf ("%d", &rows);
+    
+    printPattern (rows);
+
+    
+}
