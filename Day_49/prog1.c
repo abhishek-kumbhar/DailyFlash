dd7d47d3a1363e5d698a2afec0348de1fb245e74
Abhishek Kumbhar <57053104+abhishek-kumbhar@users.noreply.github.com>
2020-05-06 07:15:00 +0530

    Create prog1.c

---
+++ Day_49/prog1.c
@@ -0,0 +1,26 @@
+/*
+
+Program 1: Write a Program that print multiplication of Series up to nth length if user provides length.
+
+Series : (1*1) * (2*2) * (3*3) * ... * (n*n).
+
+
+*/
+
+# include <stdio.h>
+
+void main (void) {
+
+    int x, sum = 1;
+
+    printf ("\nEnter no of terms : ");
+    scanf ("%d", &x);
+
+    for (int i = 1; i < x + 1; i++) {
+        sum *= i * i;
+    }
+
+    printf ("\nThe multiplication of Series of length %d = %d\n", x, sum);
+
+
+}   
