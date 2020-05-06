6ed797d1f60f15833f24070dcb766ce780ed22e2
Abhishek Kumbhar <57053104+abhishek-kumbhar@users.noreply.github.com>
2020-05-06 07:16:22 +0530

    Create prog5.c

---
+++ Day_49/prog5.c
@@ -0,0 +1,41 @@
+/*
+
+Program 5: Write a Program to Print following Pattern.
+
+                * * * * * * * * * 
+                  *           *   
+                    *       *     
+                      *   *       
+                        *         
+    
+
+
+*/
+
+# include <stdio.h>
+
+
+void printPattern (int rows) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < (2 * rows - 1); j++) {
+            if (i + j == 2 * i || i + j == rows * 2 - 2 || i == 0) {
+                printf ("* ");
+            } else {
+                printf ("  ");
+            } 
+        }
+        printf ("\n");
+    }
+
+}
+
+
+void main (void) {
+
+    int rows;
+    printf ("\nEnter number of Rows : ");
+    scanf ("%d", &rows);
+
+    printPattern (rows);
+
+}
