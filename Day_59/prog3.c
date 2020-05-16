bbdfd31da6cbeee6d7ccdfc08c1e1078d874c2b3
Abhishek Kumbhar <57053104+abhishek-kumbhar@users.noreply.github.com>
2020-05-16 22:00:10 +0530

    Create prog3.c

---
+++ Day_59/prog3.c
@@ -0,0 +1,29 @@
+/*
+ 
+Program 3: Write a Program that accepts a Array on Length N from user and
+           prints multiplication of all even elements from that array.
+
+
+*/
+
+# include <stdio.h>
+
+void main (void) {
+    
+    int x, array [20], mult = 1;
+    
+    printf ("\nEnter Length of array : ");
+    scanf ("%d", &x);
+
+    printf ("\nEnter the elements of Array : ");
+
+    for (int i = 0; i < x; i++) {
+        scanf ("%d", &array [i]);
+        if (array [i] % 2 == 0) {
+            mult *= array [i];
+        }
+    }
+    
+    printf ("\nMultiplication of Even elements from array = %d\n", mult);
+
+}
