a4de9f5537de64f234f12b61b0aff2b192470b7c
Abhishek Kumbhar <57053104+abhishek-kumbhar@users.noreply.github.com>
2020-05-18 22:28:04 +0530

    Update prog1.c

--- Day_60/prog1.c
+++ Day_60/prog1.c
@@ -3,7 +3,7 @@
 Program 1: Write a Program that print Addition of Series up to nth length if
            user provides length.
 
-Series:    1*(x+y)/1! + 2*(x+y)/2! + 3*(x+y)/3! + ... + n*(x+y)/n!
+Series:    1/1! + 1/2! + 1/3! + ... + 1/n!
 
 
 */
@@ -16,22 +16,16 @@ int fact (int i) {
 
 void main (void) {
     
-    int n, x, y;
+    int n;
     float sum = 0;
     
     printf ("\nEnter length of Series : ");
     scanf ("%d", &n);
 
-    printf ("\nEnter X : ");
-    scanf ("%d", &x);
-    
-    printf ("\nEnter Y : ");
-    scanf ("%d", &y);
-
     for (int i = 1; i < n + 1; i++) {
-        sum +=  i * (x + y)/ (float) fact (i);
+        sum += 1.0 / fact (i);
     }
 
-    printf ("\nThe addition of Series of length %d = %.1f", n, sum);
+    printf ("\nThe addition of Series of length %d = %.2f", n, sum);
 
 }
