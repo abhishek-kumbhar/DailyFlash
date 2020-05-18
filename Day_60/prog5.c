718aef51740ccbe017b70b93fe46b52d7bf09e6a
Abhishek Kumbhar <57053104+abhishek-kumbhar@users.noreply.github.com>
2020-05-18 22:29:45 +0530

    Update prog5.c

--- Day_60/prog5.c
+++ Day_60/prog5.c
@@ -1,7 +1,16 @@
 /*
 
-Program 5: Write a Program calculate Length (L) of a simple pendulum if user
-           provides the period (T) of that pendulum in seconds.
+Program 5: Write a Program calculate period of a simple pendulum (T) if user
+           provides the Length of pendulum in (L) in meters.
+        
+           {Steps: To calculate Period of simple pendulum we can use formula
+                T = 2π * √L/g
+                Where,
+                  L: is length of pendulum in Meters.
+                  g: is acceleration but we can simply use gravitation constant
+                     since gravitational force acts on it. So (g = 9.81).
+                     π = 3.142
+           }
 
 
 */
@@ -9,19 +18,18 @@ Program 5: Write a Program calculate Length (L) of a simple pendulum if user
 # include <stdio.h>
 # include <math.h>
 
-#define PI 3.142
-#define g 9.81
+# define PI 3.142
+# define g 9.81
 
 void main (void) {
     
     float length, period;
     
-    printf ("\nEnter Period of pendulum (in seconds) : ");
-    scanf ("%f", &period);
+    printf ("\nEnter Length of Pendulum (in meter) : ");
+    scanf ("%f", &length);
     
-    length = g * (period * period) / (4 * (PI * PI));
+    period = 2 * PI * sqrt ((length / g));
 
-    printf ("\nThe length of pendulum = %.2f meter\n", length);
-
+    printf ("\nThe period of pendulum = %.3f seconds.\n", period);
 
 }
