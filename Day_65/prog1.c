2bad6259c0f84f0080f5d1228faab411bd800f6f
Abhishek Kumbhar <57053104+abhishek-kumbhar@users.noreply.github.com>
2020-05-22 23:21:47 +0530

    Create prog1.c

---
+++ Day_65/prog1.c
@@ -0,0 +1,39 @@
+/*
+ 
+Program 1: Write a Program that print Addition of Series up to nth length if
+           user provides length.
+
+Series:    1/(x+y)^1*1! + 2/(x+y)^2*2! + 3/(x+y)^3*3! + ... + n/(x+y)^n*n!
+
+
+*/
+
+
+# include <stdio.h>
+# include <math.h>
+
+int fact (int i) {
+    return i == 0 ? 1 : i * fact (i - 1);
+}
+
+void main (void) {
+    
+    int n, x, y;
+    float sum = 0;
+    
+    printf ("\nEnter length of Series : ");
+    scanf ("%d", &n);
+
+    printf ("\nEnter X : ");
+    scanf ("%d", &x);
+    
+    printf ("\nEnter Y : ");
+    scanf ("%d", &y);
+
+    for (int i = 1; i < n + 1; i++) {
+        sum +=  i / pow((x+y), i) * fact (i);
+    }
+
+    printf ("\nThe addition of Series of length %d = %.2f", n, sum);
+
+}
